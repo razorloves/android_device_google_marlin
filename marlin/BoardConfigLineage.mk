af972d5a3fa5c2965503b9d41a363af5c9605042
razorloves <razorloves@gmail.com>
2018-12-02 22:09:03 -0600

    marlin: Add prebuilt vendor img to build
    Change-Id: I8a778d00a04e4c527648accb1b9999695be88fe5

--- marlin/BoardConfigLineage.mk
+++ marlin/BoardConfigLineage.mk
@@ -1,2 +1,5 @@
 # BoardConfig for lineage_marlin
+
+BOARD_PREBUILT_VENDORIMAGE := vendor/google/marlin/proprietary/vendor.img
+
 include device/google/marlin/BoardConfigLineage.mk
--- sailfish/BoardConfigLineage.mk
+++ sailfish/BoardConfigLineage.mk
@@ -1,2 +1,5 @@
 # BoardConfig for lineage_sailfish
+
+BOARD_PREBUILT_VENDORIMAGE := vendor/google/sailfish/proprietary/vendor.img
+
 include device/google/marlin/BoardConfigLineage.mk
